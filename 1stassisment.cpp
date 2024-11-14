3cc001000f2a3238b9164cef93a703f401fa2c56
fax9 <hashmihamza894@gmail.com>
2024-11-14 20:16:17 +0500

    adding employ

--- 1stassisment.cpp
+++ 1stassisment.cpp
@@ -8,3 +8,18 @@ endm
 .model small
 .stack 100h
 .data
+
+; Counter variables for product and employee entries
+numb dw 0        
+emp dw 0          
+
+; Product data arrays
+product_names db 100*32 dup(' ')  
+product_ids dw 100 dup(0)          
+product_quantities dw 100 dup(0)  
+product_prices dw 100 dup(0)       
+
+; Employee data arrays
+employee_names db 100*32 dup(' ')  
+employee_ids dw 100 dup(0)         
+employee_numbers dw 100 dup(0)     
